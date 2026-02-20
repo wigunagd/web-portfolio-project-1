739303917f920bbededaf7db35cf5bfb3115ba16
Gede Wiguna <wiguna.gd@gmail.com>
2026-02-20 23:41:10 +0800

    2026-02-20

--- src/pagesections/Contact.tsx
+++ src/pagesections/Contact.tsx
@@ -50,7 +50,6 @@ const Contact = () => {
         // eslint-disable-next-line react-hooks/set-state-in-effect
         setMounted(true)
     }, []);
-    
 
     const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
